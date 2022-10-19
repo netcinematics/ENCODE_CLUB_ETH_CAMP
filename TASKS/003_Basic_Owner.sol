5f5cd4f4cf255c53cb92cf1ba40099c34ba96200
NetCinematics <alpinefalcon@protonmail.com>
2022-10-19 16:18:07 -0700

    003

--- TASKS/003_Basic_Owner.sol
+++ TASKS/003_Basic_Owner.sol
@@ -9,6 +9,21 @@ contract BootcampContract {
     //TODO  add variable address of deployer, and contructor setting it, and external function to
     // return 0x00...dEaD. if called by deployer, else if not owner return deployers address.
 
+    address owner;
+    address dead = 0x000000000000000000000000000000000000dEaD ;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function testOwner() external view returns(address){
+        if(msg.sender == owner){
+            return dead;
+        } else {
+            return owner;
+        }
+    }
+
     function store(uint256 num) public {
         number = num;
     }
