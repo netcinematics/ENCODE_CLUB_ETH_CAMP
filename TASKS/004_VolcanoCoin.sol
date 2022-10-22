7a1ce25b080cc9d96bcfc526dc2cccf25665f40b
NetCinematics <alpinefalcon@protonmail.com>
2022-10-21 19:16:25 -0700

    fix transaction

--- TASKS/004_VolcanoCoin.sol
+++ TASKS/004_VolcanoCoin.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.17;
 
+import "hardhat/console.sol";
 
 contract VolcanoCoin {
 
@@ -13,11 +14,14 @@ contract VolcanoCoin {
     // address dead = 0x000000000000000000000000000000000000dEaD ;
 
     constructor() {
+        console.log("constructing VOLCANOOOO COOOOIN!");
         owner = msg.sender;
         balances[owner] = totalSupply;  //MAP EXECUTION
+        console.log("VolcanoCoin is alive!");
     }
 
     modifier onlyOwner {
+        console.log("ONLYOWNER");
         require( (owner == msg.sender) , 'must be owner');
         // revert('broke');
         _;
@@ -29,7 +33,7 @@ contract VolcanoCoin {
     }
 
     //TIP: map of user address to array of Payment structs
-    mapping (address => Payment) userPayments;
+    mapping (address => Payment) public userPayments;
 
     event totalSupplyChanged( uint256 );
     event transferEvent( uint256, address );
@@ -38,7 +42,8 @@ contract VolcanoCoin {
         return owner;
     }
 
-    function setSupply() onlyOwner public { 
+    function setSupply() onlyOwner public {
+        console.log("SETSUPPLY"," should be only owner!"); 
         uint256 checkNum = totalSupply + tgt;     
         emit totalSupplyChanged(checkNum);
         totalSupply += tgt;
@@ -53,9 +58,51 @@ contract VolcanoCoin {
     }
 
     function transfer( uint256 _amt, address _toaddr) public {
-        //TIP: senders address is msg.sender
-        //TIP: senders address as parameter would be... send money to self?
+        console.log("TRANSFER"," emit event and add coin to addr if found, else add"); 
+        assert(_amt>0);
+        console.log("PRE SEND SELF check",_toaddr,msg.sender,_toaddr != msg.sender);
+        assert(_toaddr != msg.sender); //ERROR: SEND TO SELF, auto error and revert.
+        // console.log("PRE Overdraft")
+        // assert(_amt>totalSupply-_amt); //ERROR: OVERDRAWN SUPPLY
+        // Payment(_amt,_toaddr);
+        // address dead = 0x000000000000000000000000000000000000dEaD ;
+        if(balances[_toaddr]>0){
+            console.log("Pay Existing addr");
+            totalSupply -= _amt;
+            balances[_toaddr] += _amt; //PAY EXISTING
+        } else {
+            console.log("Pay new addr");
+            totalSupply -= _amt;
+            balances[_toaddr] = _amt; //PAY NEW ADDR
+        }
+
         emit transferEvent(_amt,_toaddr);
     }
+
+    // function seePayments() public view returns (Payment){
+    //     return Payment;
+    // }
+
+    //TEST-SCRIPT:
+    //getTotalSupply = 10,000 ? TRUE
+    //getOwner = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 ? TRUE
+    //balances: mapping to address = 10,000 ? TRUE
+    //transfer: ERROR: PAY SELF, LOG and REVERT ? TRUE
+    //transfer: PAY NEW ADDR 0xdead ? TRUE
+    //transfer: PAY EXISTING 0xdead ? TRUE
+    //setSupply: ERROR: NON OWNER: assert/revert ?
+    //setSupply: EVENT: totalSupplyChanged ?
+    //setSupply: by OWNER: onlyOwner ?
+    //setPayments: from transfer
+    //seePayments: fn
+
+    //console.log: ? TRUE
+    //event: ? TRUE
+    //require: ? FALSE
+    //revert: ? TRUE
+    //assert: ? TRUE
+    //customerror: ? FALSE
+
+    //SEARCH for EXAMPLES of this in DOCS... (after struggle) : )
 
 }
