278b218f10391b90fe7f39c7de221136ae0a857d
NetCinematics <alpinefalcon@protonmail.com>
2022-10-26 14:30:33 -0700

    007

---
+++ TASKS/007_VolcanoAddons.sol
@@ -0,0 +1,21 @@
+/********************************
+
+INSTRUCTIONS: 
+
+1. We made a payment mapping, but we havenʼt added all the functionality for it yet.
+Write a function to view the payment records, specifying the user as an input.
+What is the difference between doing this and making the mapping public ?
+
+
+2. For the payments record mapping, create a function called recordPayment that
+takes
+senderAdd, ReceiverAdd, AMT, 
+
+- AMT: as an input, then creates a new payment record and adds the new record to the
+userʼs payment record.
+
+3. Each time we make a transfer of tokens, we should call the this recordPayment
+function to record the transfer
+
+//PASTE INTO REMIX...
+\**************************************/
